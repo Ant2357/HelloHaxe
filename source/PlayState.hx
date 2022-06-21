0ce704b001d37db6215b543e0e3ed18b999d4e21
Ant2357 <ryu4979@gmail.com>
2022-06-21 22:45:13 +0900

    feat: init

---
+++ source/PlayState.hx
@@ -0,0 +1,20 @@
+package;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		var text = new flixel.text.FlxText(0, 0, 0, "OCHINCHINTALE", 64);
+		text.screenCenter();
+		add(text);
+
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
